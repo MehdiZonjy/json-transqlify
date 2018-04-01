5c40241060dd3fbefbe32a0926521e91ece755bb
Mehdi Zonjy <mehdizonjy@hotmail.com>
2018-04-01 17:58:36 +0800

    adds batch insert loader

--- examples/batch-insert-example/index.js
+++ examples/batch-insert-example/index.js
@@ -1,5 +1,5 @@
 const createFactory = require('../../lib').createFactory;
-require('../../lib').setLogLevel('verbose')
+
 const db = {
   host: 'localhost',
   user: 'root',
@@ -13,11 +13,11 @@ const transqlifier = factory.createTransqlifier('./insert-users.yaml');
 
 const obj =[
   { name: "Harry Potter", age: 10, address: { city: 'UK', country: 'Little Whinging' } },
-  { name: "Harry Potter", age: 10, address: { city: 'UK', country: 'Little Whinging' } },
-  { name: "Harry Potter", age: 10, address: { city: 'UK', country: 'Little Whinging' } },
-  { name: "Harry Potter", age: 10, address: { city: 'UK', country: 'Little Whinging' } },
-  { name: "Harry Potter", age: 10, address: { city: 'UK', country: 'Little Whinging' } },
-  { name: "Harry Potter", age: 10, address: { city: 'UK', country: 'Little Whinging' } }
+  { name: "Harry Potter", age: 20, address: { city: 'UK', country: 'Little Whinging' } },
+  { name: "Harry Potter", age: 30, address: { city: 'UK', country: 'Little Whinging' } },
+  { name: "Harry Potter", age: 11, address: { city: 'UK', country: 'Little Whinging' } },
+  { name: "Harry Potter", age: 12, address: { city: 'UK', country: 'Little Whinging' } },
+  { name: "Harry Potter", age: 13, address: { city: 'UK', country: 'Little Whinging' } }
 ] ;
 
 transqlifier(obj).then(factory.closePool);
